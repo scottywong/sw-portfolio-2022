065eb15c3aa98323664a59df269296e7d0b591b6
scottwong <wong.sctt@gmail.com>
2022-12-26 16:20:20 -0800

    Updates

---
+++ precache-manifest.0d61ee8aae88f67ef69e22b300fd1834.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6c9f01119fefe54c87f5e469d96fc7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "899cca5962e3edd83ee2",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "46b429a1de9b200ca247",
+    "url": "./static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "899cca5962e3edd83ee2",
+    "url": "./static/js/2.272da615.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.272da615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b429a1de9b200ca247",
+    "url": "./static/js/main.9aab8783.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
